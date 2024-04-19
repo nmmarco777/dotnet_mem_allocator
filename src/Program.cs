87a9baa469f70c75368240599957fa11a5d83377
Nate Morris <nate.morris@marconet.com>
2024-04-19 12:49:06 -0500

    feat: add spiky memory usage

--- src/Program.cs
+++ src/Program.cs
@@ -7,9 +7,9 @@ using Microsoft.Extensions.Configuration;
 
 namespace dotnet_mem_allocator;
 
-class Program
+static class Program
 {
-    const double MB_FACTOR = 1024 * 1024;
+    const int MB_FACTOR = 1024 * 1024;
 
     static void Main(string[] args)
     {
@@ -21,11 +21,13 @@ class Program
             Environment.Exit(0);
         };
 
-        int iAllocBytes = config.GetValue<int>("alloc_mb", 10) * 1000000;
-        int iSleepMs = config.GetValue<int>("sleep", 1000);
-        bool bFree = config.GetValue<bool>("free", true);
-        bool bForceFullCollection = config.GetValue<bool>("force_full_collection", false);
-        int iCollect = config.GetValue<int>("collect", 0);
+        var maxAlloc = config.GetValue("max_alloc", 10 * MB_FACTOR);
+        var minAlloc = config.GetValue("min_alloc", 10);
+        var maxSize = config.GetValue("max_size", 500 * MB_FACTOR);
+        var sleepMs = config.GetValue("sleep", 250);
+        var leak = config.GetValue("leak", false);
+        var forceFullCollection = config.GetValue("force_full_collection", false);
+        var collect = config.GetValue("collect", 0);
 
 #if CLIENT_GC
         Console.WriteLine("server GC: false");
@@ -34,21 +36,31 @@ class Program
         Console.WriteLine("server GC: true");
 #endif
 
-        Console.WriteLine("allocated bytes: " + iAllocBytes);
-        Console.WriteLine("sleep ms: " + iSleepMs);
-        Console.WriteLine("free: " + bFree);
-        Console.WriteLine("force full collection: " + bForceFullCollection);
-        Console.WriteLine("run GC every: " + iCollect);
+        Console.WriteLine("Max allocated bytes: " + maxAlloc.ToString("N0"));
+        Console.WriteLine("Min allocated bytes: " + minAlloc.ToString("N0"));
+        Console.WriteLine("Max total in-use bytes: " + maxSize.ToString("N0"));
+        Console.WriteLine("Simulate a memory leak: " + leak);
+        Console.WriteLine("Sleep ms: " + sleepMs.ToString("N0"));
+        Console.WriteLine("Force full collection: " + forceFullCollection);
+        if (collect > 0)
+        {
+            Console.WriteLine("Run GC every X cycles: " + collect);
+        }
+        Console.WriteLine("\npress Ctrl+C to exit\n");
+
+        long trackedTotalMemory = 0;
 
         try
         {
             var rand = new Random();
-            List<byte[]> m_arrays = new List<byte[]>();
+            List<byte[]> storedArrays = new List<byte[]>();
 
             int iLoop = 0;
             while (true)
             {
-                var array = new byte[iAllocBytes];
+                var allocationSize = AllocationSize(minAlloc, maxAlloc, maxSize, rand);
+                trackedTotalMemory += allocationSize;
+                var array = new byte[allocationSize];
                 var value = (byte)rand.Next();
                 Parallel.ForEach(
                     array,
@@ -59,18 +71,37 @@ class Program
                 );
                 rand.NextBytes(array);
 
-                if (!bFree)
-                    m_arrays.Add(array);
+                long deallocated = 0;
+                if (!leak)
+                {
+                    for (int i = 0; i < storedArrays.Count; i++)
+                    {
+                        if (rand.Next(0, 100) > 80)
+                        {
+                            deallocated += storedArrays[i].Length;
+                            storedArrays.RemoveAt(i);
+                        }
+                    }
+                }
+                trackedTotalMemory -= deallocated;
 
-                if (iCollect > 0 && iLoop % iCollect == 0)
+                storedArrays.Add(array);
+
+                if (collect > 0 && iLoop % collect == 0)
                 {
                     Console.WriteLine("run GC");
                     GC.Collect();
                 }
 
-                Console.WriteLine(AllocatedBytes() + TotalMemory() + CollectionCount());
+                Console.WriteLine(
+                    TotalMemory()
+                        + TrackedMemory(trackedTotalMemory)
+                        + Allocated(array)
+                        + Deallocated(deallocated)
+                        + CollectionCount()
+                );
 
-                Thread.Sleep(iSleepMs);
+                Thread.Sleep(sleepMs);
                 iLoop++;
             }
         }
@@ -85,14 +116,49 @@ class Program
         //Console.WriteLine("\tsleep: number of ms to sleep, default 1000 ms");
     }
 
-    private static string ToMb(double value) => (value / MB_FACTOR).ToString("N1") + " MB";
+    private static long AllocationSize(int minAlloc, int maxAlloc, long maxSize, Random rand)
+    {
+        const int logBase = 1024;
 
-    private static string AllocatedBytes() =>
-        "AllocatedBytesForCurrentThread: "
-        + ToMb(GC.GetAllocatedBytesForCurrentThread()).PadRight(12);
+        if (rand.Next(0, 100) < 2)
+        {
+            return Math.Max(0, (maxSize / 2) - GC.GetTotalMemory(false));
+        }
+
+        int minExponent = (int)Math.Floor(Math.Log(minAlloc, logBase));
+        int maxExponent = (int)Math.Floor(Math.Log(maxAlloc, logBase));
+        int exponent = rand.Next(minExponent, maxExponent + 1);
+
+        int root = rand.Next(0, 100) + 1;
+
+        return root * (long)Math.Pow(logBase, exponent);
+    }
 
     private static string TotalMemory() =>
-        "TotalMemory: " + ToMb(GC.GetTotalMemory(false)).PadRight(12);
+        "GC Total Memory: " + HumanReadableSize(GC.GetTotalMemory(false)).PadRight(12);
+
+    private static string TrackedMemory(long trackedTotalMemory) =>
+        "Tracked Memory: " + HumanReadableSize(trackedTotalMemory).PadRight(12);
+
+    private static string Allocated(byte[] array) =>
+        "Allocated: " + HumanReadableSize(array.Length).PadRight(12);
+
+    private static string Deallocated(long deallocated) =>
+        "Deallocated: " + HumanReadableSize(deallocated).PadRight(12);
+
+    private static string HumanReadableSize(long size)
+    {
+        string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+        double len = size;
+        int order = 0;
+        while (len >= 1024 && order < sizes.Length - 1)
+        {
+            order++;
+            len = len / 1024;
+        }
+
+        return $"{len:N2} {sizes[order]}";
+    }
 
     private static string CollectionCount()
     {
